81b96d701c5558ca5746b7f6666dfac0cfc7e842
xSyki <mikolajsykula@gmail.com>
2025-06-14 11:24:09 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.println("Hello from ESP32!");
+
+  pinMode(2, OUTPUT);
+
+  Serial.println("Pin setup")
+}
+
+void loop() {
+  Serial.println("ESP32 is running...");
+
+  digitalWrite(2, HIGH);
+
+  Serial.println("Diod HIGH")
+
+  delay(500);
+
+  digitalWrite(2, LOW);
+
+  Serial.println("Diod LOW")
+
+  delay(500);
+}
