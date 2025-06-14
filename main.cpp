60ed852e304bcd18a7416c05abe36991156fdf0b
xSyki <mikolajsykula@gmail.com>
2025-06-14 13:24:48 +0200

    feat: working test build in diod

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 void setup() {
+  delay(2000);
+  Serial.begin(115200);
+
   Serial.println("Hello from ESP32!");
 
   pinMode(2, OUTPUT);
 
-  Serial.println("Pin setup")
+  Serial.println("Pin setup");
 }
 
 void loop() {
@@ -11,13 +14,13 @@ void loop() {
 
   digitalWrite(2, HIGH);
 
-  Serial.println("Diod HIGH")
+  Serial.println("Diod HIGH");
 
   delay(500);
 
   digitalWrite(2, LOW);
 
-  Serial.println("Diod LOW")
+  Serial.println("Diod LOW");
 
   delay(500);
 }
