a77c6d696ce8664ff182519259adfb4417427630
xSyki <mikolajsykula@gmail.com>
2025-06-14 14:35:07 +0200

    feat: oled hello world

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+
+#define OLED_MOSI   23
+#define OLED_CLK    18
+#define OLED_CS     5
+#define OLED_DC     27
+#define OLED_RESET  14
+
+Adafruit_SSD1306 display(128, 64, &SPI, OLED_DC, OLED_RESET, OLED_CS);
+
 void setup() {
   delay(2000);
   Serial.begin(115200);
 
-  Serial.println("Hello from ESP32!");
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0)) {
+    Serial.println(F("OLED failed to initialize"));
+    for (;;);
+  }
 
-  pinMode(2, OUTPUT);
+  Serial.println(F("OLED initialized!"));
+  
+  display.clearDisplay();
 
-  Serial.println("Pin setup");
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 20);
+  display.println("Hello, OLED!");
+  
+  display.display();
 }
 
 void loop() {
-  Serial.println("ESP32 is running...");
 
-  digitalWrite(2, HIGH);
-
-  Serial.println("Diod HIGH");
-
-  delay(500);
-
-  digitalWrite(2, LOW);
-
-  Serial.println("Diod LOW");
-
-  delay(500);
 }
